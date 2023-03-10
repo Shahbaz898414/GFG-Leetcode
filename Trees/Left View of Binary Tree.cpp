ae56ee599157a9d454b89ea0080fdd1d251b103c
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-10 19:19:14 +0530

    10-3-23

--- Trees/Left View of Binary Tree.cpp
+++ Trees/Left View of Binary Tree.cpp
@@ -132,31 +132,3 @@ vector<int> leftView(Node *root)
 }
 
 
-/*
-void solve(TreeNode* root, vector<int> &ans, int level)
-    {
-        // base case
-        if (root == NULL)
-        {
-            return ; 
-        }
-
-        // we entered a new level 
-        if (level == ans.size())
-        {
-            ans.push_back(root -> val) ; 
-        }
-
-        solve(root -> right, ans, level + 1) ; 
-        solve(root -> left, ans, level + 1 ) ; 
-    }
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans ; 
-        int level = 0 ; 
-
-        solve(root, ans, level) ;
-        
-        return ans ; 
-    }
-
-*/
