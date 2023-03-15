aa084cafa67f0fdf412993da593407c4e1b87c8b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-15 18:16:38 +0530

    3

---
+++ String/Special Palindrome Substrings.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+    //Complete the function and return minimum number of operations
+    public:
+    int specialPalindrome(string s1, string s2){
+        //Code Here
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s1,s2;
+        cin>>s1>>s2;
+        Solution obj;
+        int ans = obj.specialPalindrome(s1,s2);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
