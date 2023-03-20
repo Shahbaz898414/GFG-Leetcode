34427b223a9fadd23826d0fcb6c72fbf5b85e441
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-20 19:04:45 +0530

    1

---
+++ POTD/Can Place Flowers.cpp
@@ -0,0 +1,11 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        
+    }
+};
