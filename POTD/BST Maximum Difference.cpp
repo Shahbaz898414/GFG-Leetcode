17e01fe227f982db3f5caff93e423cf6b6e8df41
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-23 20:02:53 +0530

    1

---
+++ POTD/BST Maximum Difference.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int maxDifferenceBST(Node *root,int target){
+        // Code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int target;
+        cin >> target;
+        Solution ob;
+        cout << ob.maxDifferenceBST(root, target);
+        cout << endl;
+    }
+}
+// } Driver Code Ends
