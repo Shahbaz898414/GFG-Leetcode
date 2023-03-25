164dee7840eb0319564ee9c1b658b7ce13489de6
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-25 18:07:05 +0530

    1

---
+++ POTD/Remove and Reverse.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution {
+  public:
+    string removeReverse(string S) {
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin>>S;
+        Solution obj;
+        auto ans = obj.removeReverse(S);
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
