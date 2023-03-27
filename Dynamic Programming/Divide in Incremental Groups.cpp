0d51b9f396670d1a6bd5f69ce4d6c819bb671478
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-27 19:09:37 +0530

    1

---
+++ Dynamic Programming/Divide in Incremental Groups.cpp
@@ -0,0 +1,27 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+class Solution {
+  public:
+    int countWaystoDivide(int N, int K) {
+        // Code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        Solution ob;
+        cout << ob.countWaystoDivide(N, K) << endl;
+    }
+}
+// } Driver Code Ends
