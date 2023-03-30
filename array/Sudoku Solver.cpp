d84f23cf035b64a233b11b510f9290e24f1b0e52
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-30 18:15:47 +0530

    1

---
+++ array/Sudoku Solver.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+
+bool isSafe(int row,int col,char &c,vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c) 
+                return false;
+            if(board[row][i]==c) 
+                return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+                return false;
+        }
+        return true;
+}
+
+bool solve(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){ // Traversal through each element in 9*9 Sudoku
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';c++){
+                        if(isSafe(i,j,c,board)==true){ //Checking whether 1-9 which numbers sets the 3 Sudoku Criteria
+                            board[i][j]=c;
+                            if(solve(board)==true) //Condition Checking
+                                return true;
+                            else
+                                board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+}
+
+void solveSudoku(vector<vector<char>>& board) {
+        solve(board);        
+}
+
+
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.minimumInteger(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
--- array/test.cpp
+++ array/test.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define ll long long
     
-    bool isValidSudoku(vector<vector<char> > &board) {	
+bool isValidSudoku(vector<vector<char> > &board) {	
         int row[9][9] = {0}, col[9][9] = {0}, grid_3x3[9][9] = {0}; 
         for(int i = 0; i < board.size(); i++)
           for(int j = 0; j < board[i].size(); j++)
@@ -10,18 +10,12 @@ using namespace std;
                     int num = board[i][j] - '1', k = i/3*3+j/3; 
                     if(row[i][num] || col[j][num] || grid_3x3[k][num])
                         return false;
-
                     row[i][num] = col[j][num] = grid_3x3[k][num] = 1;
                 }
         
         return true;
 
-    }
-
-
-
-
-   
+}
 
 
 int main() {
