4899f3be57b7cefa0b5509823a7871bc8bb6791a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-30 15:44:43 +0530

    1

--- array/Valid Sudoku.cpp
+++ array/Valid Sudoku.cpp
@@ -5,46 +5,37 @@ using namespace std;
 
 int main() {
 
-  ll n,m;cin>>n>>m;
-  ll a[n];
-  
-  for(ll i=0; i < n; i++) {
-    cin>>a[i];
-  }
+    ll n,m;cin>>n>>m;
+    ll a[n];
+    
+    for(ll i=0; i < n; i++) {
+      cin>>a[i];
+    }
 
-  sort(a,a+n);
+    sort(a,a+n);
 
-  int closest=a[0]+a[1]+a[2];
+    int closest=a[0]+a[1]+a[2];
 
-  for(int i=0;i<n-2;i++){
-    int l=i+1;
-    int r=n-1;
-    while(l<r){
-      int cur=a[i]+a[l]+a[r];
-      if(cur==m)  return cur;
-      if(abs(m-cur)<abs(m-closest)){
-        closest=cur;
-      }
+    for(int i=0;i<n-2;i++){
+      int l=i+1;
+      int r=n-1;
+      while(l<r){
+        int cur=a[i]+a[l]+a[r];
+        if(cur==m)  return cur;
+        if(abs(m-cur)<abs(m-closest)){
+          closest=cur;
+        }
 
 
-      if(m<cur){
-        r--;
-      }else {
-        l++;
+        if(m<cur){
+          r--;
+        }else {
+          l++;
+        }
       }
     }
-  }
 
 
-  return closest;
+    cout<< closest<<endl;
 
-  // ll l=0,r=n-1;
-
-  
-
-  
- 
-
-  
-  
 }
