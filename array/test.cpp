732ad3839f054403d405670067fb655abd195f80
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-30 15:53:21 +0530

    1

--- array/test.cpp
+++ array/test.cpp
@@ -23,29 +23,28 @@ using namespace std;
 
 
 
-    bool isValidSudoku(vector<vector<char>>& board)  {
-        vector<set<char>> rows(9), cols(9), blck(9);                
+    // bool isValidSudoku(vector<vector<char>>& board)  {
+    //     vector<set<char>> rows(9), cols(9), blck(9);                
         
-        for (int i = 0; i < 9; i++) 
-            for (int j = 0; j < 9; j++) 
-                if (board[i][j] != '.') {
-                    int d = board[i][j], b = (i/3)*3+j/3;
-                    if (!rows[i].emplace(d).second) return false;   
-                    if (!cols[j].emplace(d).second) return false;  
-                    if (!blck[b].emplace(d).second) return false;   
-                }
-        return true;
-    }
+    //     for (int i = 0; i < 9; i++) 
+    //         for (int j = 0; j < 9; j++) 
+    //             if (board[i][j] != '.') {
+    //                 int d = board[i][j], b = (i/3)*3+j/3;
+    //                 if (!rows[i].emplace(d).second) return false;   
+    //                 if (!cols[j].emplace(d).second) return false;  
+    //                 if (!blck[b].emplace(d).second) return false;   
+    //             }
+    //     return true;
+    // }
 
 
 int main() {
 
-  ll t;cin>>t;
-  while(t--) {
-    ll n;cin>>n;
+ 
+    int n;cin>>n;
     string s1,s2;cin>>s1>>s2; 
     
-  }
+
 
   
   
